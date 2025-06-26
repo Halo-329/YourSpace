8268a204f35ce59f829b794115a782f51d54ebf0
halo <dongjin778@naver.com>
2025-06-26 14:41:26 +0900

    feat : JwtUtill Class 생성 1. 키 발급 2. JWT 생성 3. JWT 까주는 함수 (유저가 토큰 보내면 서버에서 열어봐야 하니까)

--- src/main/java/com/apple/shop/global/config/SecurityConfig.java
+++ src/main/java/com/apple/shop/global/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -20,14 +21,23 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf((csrf) -> csrf.disable());
+
+
+        // 세션데이터 생성 하지 말아주세요.
+        http.sessionManagement((session) -> session
+            .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+    );
+
         http.authorizeHttpRequests((authorize) ->
                 authorize.requestMatchers("/**").permitAll()
         );
-        http.formLogin((formLogin) -> formLogin.loginPage("/member/login")
-//                        .loginProcessingUrl("/member/check") // 로그인 처리 요청 (POST)// 1. 로그인 할 URL
-                        .defaultSuccessUrl("/item/list", true) // 2. 로그인 성공시 이동할 URL
-//                .failureUrl("/fail") // 3. 로그인 실패시 이동할 URL
-        );
+//        http.formLogin((formLogin) -> formLogin.loginPage("/member/login")
+////                        .loginProcessingUrl("/member/check") // 로그인 처리 요청 (POST)// 1. 로그인 할 URL
+//                        .defaultSuccessUrl("/item/list", true) // 2. 로그인 성공시 이동할 URL
+////                .failureUrl("/fail") // 3. 로그인 실패시 이동할 URL
+//        );
+
+
         http.logout(logout-> logout.logoutUrl("/member/logout")
                 .logoutSuccessUrl("/item/list")
         );
