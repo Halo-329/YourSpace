005c82cbab02c442e85061350b5ff12b6ea4d19c
halo <dongjin778@naver.com>
2025-07-17 13:26:11 +0900

    feat : 장바구니 UI 완성

---
+++ src/main/java/com/apple/shop/domain/cart/controller/CartController.java
@@ -0,0 +1,18 @@
+package com.apple.shop.domain.cart.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+@RequestMapping("/cart")
+public class CartController {
+
+    // https://cart.coupang.com/cartView.pang
+    @GetMapping("cartView")
+    String cartView(){
+
+        return "/cart/cartView";
+    }
+}
